ce4772bfeb6e957cebc524eb21043f0890cb8c36
Crazynuget <andydchoo@gmail.com>
2021-07-06 15:31:29 -0400

    adding lots of work

--- script.js
+++ script.js
@@ -1,43 +1,92 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 var chars = 8;
 var uppercase = false;
 var lowercase = false;
 var numbers = false;
 var specialChar = false;
+var lowerString = "abcdefghijklmnopqrstuvwxyz"
+var upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberString = "0123456789";
+var specialString = "!@#$%^&*?";
+var finalPassword = [];
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  settings();
+  finalPassword = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = finalPassword;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-if (document.getElementById('uppercase').checked) {
-  uppercase = true;
-} else {
-    uppercase = false;
+//Start a series of prompts asking for user's input on how they want their pswd
+function settings() {
+  chars = prompt("How many Characters in your password?", "Enter a number 8-128");
+  if (chars < 8) {
+    alert("Must have at least 8 Characters.");
+    return;
+  }
+  else if (chars > 128) {
+    alert("Cannot have more than 128 characters.");
+    return;
+  }
+
+  uppercaseBox = confirm("Password contains uppercase letters?(Cancel for no uppercase)");
+  lowercaseBox = confirm("Password contains lowercase?(Cancel for no lowercase)");
+  numbersBox = confirm("Password contains numbers?(Cancel for no numbers)");
+  specialCharBox = confirm("Password contains special characters?(Cancel for no special characters)");
+
+let userInput = {
+  chars : chars,
+  uppercaseBox : uppercaseBox,
+  numbersBox : numbersBox,
+  specialCharBox : specialCharBox
+}
+  return userInput;
 }
 
-if (document.getElementById('lowercase').checked) {
-  lowercase = true;
-} else {
-    lowercase = false;
-}
+//Begin dumbed down algorithm for generating a password
+function generatePassword() {
 
-if (document.getElementById('numbers').checked) {
-  numbers = true;
-} else {
-    numbers = false;
-}
+  //Generate random characters from the user's choices
+  function getCharacters() {
+  for (let i = 0; i < (chars / 4); i++) {
+  var placeChar;
+  if (uppercase == true) {
+    placeChar += upperString.charAt(Math.floor(Math.random() * upperString.length));
+    return finalPassword.push(placeChar);
+  } else {
 
-if (document.getElementById('specialChar').checked) {
-  specialChar = true;
-} else {
-    specialChar = false;
+  }
+  if (lowercase == true) {
+    placeChar += upperString.charAt(Math.floor(Math.random() * upperString.length));
+    return finalPassword.push(placeChar);
+  } else {
+
+  }
+  if (numbers == true) {
+    placeChar += numberString.charAt(Math.floor(Math.random() * numberString.length));
+    return finalPassword.push(placeChar);
+  } else {
+
+  }
+  if (specialChar == true) {
+    placeChar += specialString.charAt(Math.floor(Math.random() * specialString.length));
+    return finalPassword.push(placeChar);
+  } else {
+
+  }
+  i++;
+  }
+} //Make the password same length as user desired
+for (y = finalPassword.length; y > chars; y--) {
+  finalPassword.pop();
+} //Convert the password array into a string
+finalPassword.toString();
 }
